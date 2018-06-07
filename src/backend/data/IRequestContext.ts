21c12cda5ccd9ec9910b06bfc50e7dab375d0ba7
Ivo Katunaric <ikatun@gmail.com>
2018-06-07 10:29:09 +0200

    Add IRequestContext

---
+++ src/backend/data/IRequestContext.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from 'express';
+import { EntityManager } from 'typeorm';
+
+export interface IRequestContext {
+  em: EntityManager;
+  request?: Request;
+  response?: Response;
+}
